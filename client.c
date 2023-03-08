d29c9ac8b931f9f5c1a33ff584d73bde2124051e
Tanvir Ahmed Anas <tanas@lab2r3s1.42abudhabi.ae>
2023-03-08 19:08:04 +0400

    able to process SIGUSR1 and SIGUSR2 now

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: tanas <tanas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 14:43:39 by tanas             #+#    #+#             */
-/*   Updated: 2023/03/06 18:52:12 by tanas            ###   ########.fr       */
+/*   Updated: 2023/03/08 19:06:29 by tanas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,21 @@
 // client
 int	main(int argc, char **argv)
 {
-	int	pid;
+	int		pid;
 
-	if (argc != 2)
+	if (argc != 3)
 	{
-		printf("\033[1;3;31m\nError! Invalid number of arguments entered.\n\n");
+		ft_printf("\033[1;3;31m\nError! Invalid number of arguments entered.\n\n");
 		exit(0);
 	}
 	pid = ft_atoi(argv[1]);
-	kill(pid, SIGUSR1);
-	return (0);
+	int i = -1;
+	while (++i < 8)
+	{
+		if (argv[2][0] & 0x01 << i)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		usleep(3);
+	}
 }
--- server.c
+++ server.c
@@ -6,15 +6,22 @@
 /*   By: tanas <tanas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 22:51:26 by tanas             #+#    #+#             */
-/*   Updated: 2023/03/06 18:51:36 by tanas            ###   ########.fr       */
+/*   Updated: 2023/03/08 19:03:55 by tanas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	signal_handler(int signum)
+void	sigusr1_handler(int signum)
 {
-	ft_printf("signal %i from client received\n", signum);
+	(void) signum;
+	ft_printf("1");
+}
+
+void	sigusr2_handler(int signum)
+{
+	(void) signum;
+	ft_printf("0");
 }
 
 // server
@@ -23,9 +30,9 @@ int	main(void)
 	int	pid;
 
 	pid = getpid();
-	ft_printf("PID: %i\n", pid);
-	signal_handler(SIGUSR1);
-	for (;;)
+	ft_printf("SERVER PID: %i\n", pid);
+	signal(SIGUSR1, sigusr1_handler);
+	signal(SIGUSR2, sigusr2_handler);
+	while (1)
 		pause();
-	return (0);
 }
